a356ffaf4c61da3a00cea26ee91f4cfde5462904
Martin Ring <mail@martinring.de>
2013-05-10 17:50:20 +0200

    fixes #15

--- project/Build.scala
+++ project/Build.scala
@@ -7,11 +7,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "org.scala-lang" % "scala-swing" % "2.10.0"      
+      "org.scala-lang" % "scala-swing" % "2.10.1"      
     )
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
-      scalaVersion := "2.10.0",      
+      scalaVersion := "2.10.1",      
       lessEntryPoints <<= baseDirectory(d => 
         (d / "app" / "assets" / "stylesheets" ** "ide.less") +++
         (d / "app" / "assets" / "stylesheets" ** "login.less") +++
